f77a30ac6bd79253e41083184da2c1de8a452266
Andrey Globa <Globa.Andrey@gmail.com>
2020-04-21 13:24:37 +0300

    Init Lombok

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,5 +1,12 @@
 package ru.netology;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Radio {
 
     private int currentStation = 0;
@@ -8,71 +15,6 @@ public class Radio {
     private int currentVolume = 20;
     private int minVolume = 0;
     private int maxVolume = 100;
-
-    public Radio(){
-
-    }
-
-    public Radio(int currentStation, int maxStation, int minStation, int currentVolume, int minVolume, int maxVolume) {
-        this.currentStation = currentStation;
-        this.maxStation = maxStation;
-        this.minStation = minStation;
-        this.currentVolume = currentVolume;
-        this.minVolume = minVolume;
-        this.maxVolume = maxVolume;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public int getMaxStation() {
-        return maxStation;
-    }
-
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-
-    public int getMinStation() {
-        return minStation;
-    }
-
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        this.currentStation = currentStation;
-    }
-
-    public void setMintStation(int mintStation) {
-        this.minStation = mintStation;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        this.currentVolume = currentVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
 
     public void previousStation() {
         if (currentStation == minStation) {
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
